5971506cf91b9e998b971e6e8e447c4bfd4f0cd4
aebharath16 <aebharath17@gmail.com>
2025-08-21 15:18:44 +0000

    Add Spring Boot app and Dockerfile

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RestController
+    class HelloController {
+        @GetMapping("/")
+        public String hello() {
+            return "Hello from Cloud Run!";
+        }
+    }
+}
